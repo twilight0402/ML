33db5e7d442579804ccdbf3b0be39f43340df6c8
wangchao <twilight0402@163.com>
2019-09-12 11:42:16 +0800

    createTree

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,12 +1,13 @@
 from math import log
+import operator
 
 
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
-            [1, 1, 'yes'],
-            [1, 0, 'no'],
-            [0, 1, 'no'],
-            [0, 1, 'no']]
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
 
@@ -37,18 +38,18 @@ def splitDataSet(dataSet, axis, value):
     for line in dataSet:
         if line[axis] == value:
             newline = line[:axis]
-            newline.extend(line[axis+1:])
+            newline.extend(line[axis + 1:])
             returnDataSet.append(newline)
     return returnDataSet
 
 
 def chooseBestFeatureToSplit(dataSet):
     """寻找最好的分类特征==> 寻找分类后，信息增益最大的特征"""
-    numberOfFeature = len(dataSet[0]) -1        # 最后一位不要，因为他是分类结果
+    numberOfFeature = len(dataSet[0]) - 1  # 最后一位不要，因为他是分类结果
     numberOfDataSet = len(dataSet)
-    baseEntropy = calcShannonEnt(dataSet)       # 原始香农熵
-    bestInfoGain = 0.0                          # 用于记录最大信息熵
-    bestFeature = -1                            # 用于记录最大信息熵对应的特征下标
+    baseEntropy = calcShannonEnt(dataSet)  # 原始香农熵
+    bestInfoGain = 0.0  # 用于记录最大信息熵
+    bestFeature = -1  # 用于记录最大信息熵对应的特征下标
 
     # 对每个特征进行划分，找到划分后，信息增益最大的特征
     for i in range(numberOfFeature):
@@ -73,23 +74,54 @@ def chooseBestFeatureToSplit(dataSet):
 
 
 def majorithCnt(classList):
-    """"""
+    """第一种递归终止情况，所有的属性已经用完，但是分类结果并不一致，此时采取多数元组
+    classList是所有分类结果的列表"""
+    classCounts = {}
+    # 统计出所有labels的数量
+    for item in classList:
+        if item not in classCounts.keys():
+            classCounts[item] = 0
+        classCounts[item] += 1
+
+    # 排序classCounts不是list，不可以用sort()，这里使用sorted()排序
+    sortedClassCount = sorted(classCounts.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
 def createTree(dataSet, labels):
-    """创建决策树"""
+    """创建决策树。labels 是对每个特征值的含义的解释，方便建立决策树"""
     # 递归终止条件
+    classList = [example[-1] for example in dataSet]
+    # (1)属性已经用完
+    if len(dataSet[0]) == 1:
+        majorithCnt(classList)
+    # (2)所有分类已经一致
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
 
-    # 寻找最优特征
+    # 1. 寻找最优特征
+    bestFeature = chooseBestFeatureToSplit(dataSet)
+    bestFeatureLabel = labels[bestFeature]  # 只是标签，用于建树
+    mytree = {bestFeatureLabel: {}}  # 初始化树
+    del labels[bestFeature]  # subLabels
 
-    # 计算每个特征的所有值，
+    # 2. 当前最优特征的所有取值，去重
+    totalValues = [example[bestFeature] for example in dataSet]
+    uniqueValues = set(totalValues)
 
-    return myTree
+    # 3. 每个value一个分支，确定每个分支的值。因为是递归，所以分支下可能还有分支(字典里可能嵌套字典)，如果该分支已经可以结束，则返回返回classList中的一个(分类结果)
+    for value in uniqueValues:
+        subDataSet = splitDataSet(dataSet, bestFeature, value)
+        subLabels = labels[:]
+        mytree[bestFeatureLabel][value] = createTree(subDataSet, subLabels)
+    return mytree
 
 
 if __name__ == "__main__":
     dataSet, labels = createDataSet()
-    print(calcShannonEnt(dataSet))
-    bestFeature = chooseBestFeatureToSplit(dataSet)
-    print(bestFeature)
-    print(dataSet)
+    # print(calcShannonEnt(dataSet))
+    # bestFeature = chooseBestFeatureToSplit(dataSet)
+    # print(bestFeature)
+    # print(dataSet)
+    # print(majorithCnt(['a','a','a','b','b','b','b']))
+    print(createTree(dataSet, labels))
