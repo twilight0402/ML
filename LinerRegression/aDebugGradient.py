42be640aa40ab6c3d50181e4fe7de23883fe5b44
wangchao <twilight0402@163.com>
2019-10-04 08:50:35 +0800

    GradientDescent-over

--- LinerRegression/aDebugGradient.py
+++ LinerRegression/aDebugGradient.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from LinerRegression.model_selection import train_test_split
+# from LinerRegression.model_selection import train_test_split
 
 
-class StochasticDescent:
+class aDebugGradient:
     """随机梯度下降法"""
 
     def __init__(self):
@@ -11,38 +11,43 @@ class StochasticDescent:
         self.intercept_ = None  # 表示截距 ==>theta[0]
         self._thera = None      # 表示完整的theta==> theta[:]
 
-    def fit_SGD(self, X_train, y_train, eta=0.01, n_iters=1):
+    def fit_debug(self, X_train, y_train, eta=0.01, n_iters=1e4):
         """使用梯度下降法寻找最小的代价函数"""
         # 格式化X和theta，加上x0 和 theta0
         X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
         initial_theta = np.zeros(X_b.shape[1])
 
         # 调用循环的梯度下降
-        self._thera = self.sgd(X_b, y_train, initial_theta, n_iters, 5, 50)  # , eta=eta, n_iters=n_iters)
+        self._thera = self.gradient_Descent(self.dj_debug, X_b, y_train, initial_theta, eta, n_iters)
         self.intercept_ = self._thera[0]
         self.coef_ = self._thera[1:]
         return self
 
-    def sgd(self, X_b, y_train, initial_theta, n_iters, t0=5, t1=50):
-        """随机梯度下降, niters是轮数"""
+    def J(self, theta, X_b, y):
+        return np.sum((y - X_b.dot(theta)) ** 2) / len(y)
 
-        def get_study_rate(i_iters):
-            """把学习率和迭代次数联系起来"""
-            return t0 / (t1 + i_iters)
+    def dj_debug(self, X_b, theta, y, epsilon=0.001):
+        res = np.zeros((len(theta), ))
+        for i in range(len(theta)):
+            theta_1 = theta.copy()
+            theta_2 = theta.copy()
+            theta_1[i] += epsilon
+            theta_2[i] -= epsilon
+            res[i] = (self.J(theta_1, X_b, y) - self.J(theta_2, X_b, y)) / (2 * epsilon)
+        return res
 
-        def dJ_sgd(X_b_i, theta, y):
-            """计算随机一个元素的梯度"""
-            return 2 * X_b_i.T.dot(X_b_i.dot(theta) - y)
+    def dJ_math(self, X_b, theta, y):
+        return X_b.T.dot(X_b.dot(theta) - y) * 2. / len(y)
 
-        m = len(X_b)
+    def gradient_Descent(self, dj, X_b, y_train, initial_theta, eta, n_iters,  epsilon=1e-8):
+        """梯度下降"""
         theta = initial_theta
-        for n in range(int(n_iters)):
-            indexes = np.random.permutation(m)
-            X_b_new = X_b[indexes, :]
-            y_new = y[indexes]
-            for i in range(m):
-                sgd = dJ_sgd(X_b_new[i], theta, y_new[i])
-                theta = theta - get_study_rate(m * n + i) * sgd
+        for i in range(int(n_iters)):
+            gradient = dj(X_b, theta, y_train)      # 计算得到梯度
+            last_theta = theta
+            theta = theta - eta * gradient
+            if np.absolute(self.J(theta, X_b, y_train) - self.J(last_theta, X_b, y_train)) < epsilon:
+                break
         return theta
 
     def predict(self, x_test):
@@ -55,8 +60,7 @@ class StochasticDescent:
         return metrics.r2_score(y_test, y_test_predict)
 
 
-if __name__ == "__main__" :
-
+if __name__ == "__main__":
     from sklearn import datasets
     data = datasets.load_boston()
     X = data.data
@@ -81,12 +85,6 @@ if __name__ == "__main__" :
     # 交叉验证
     # X_train_standard, X_test_standard, y_train, y_test = train_test_split(X_standard, y, test_ratio=0.2, seed=666)
 
-    sgd = StochasticDescent()
-#    sgd.fit_SGD(X, y, n_iters=5)
-    print(sgd._thera)
-
-    from sklearn.linear_model import SGDRegressor
-
-    sgd_reg = SGDRegressor(max_iter=50)
-    sgd_reg.fit(X, y)
-    print(sgd_reg.coef_, sgd_reg.intercept_)
+    sgd = aDebugGradient()
+    sgd.fit_debug(X, y)
+    print(sgd.intercept_, " ,", sgd.coef_)
